aa5471a9c6bb25a60841e38a9b685ddfdb079598
Keith To <keithtkto@gmail.com>
2016-04-08 09:27:28 -0700

    hide env

--- tasks/send_reminders.js
+++ tasks/send_reminders.js
@@ -10,8 +10,8 @@ var User = require("../models/user");
 
 // Twilio Credentials
 var accountSid = "ACba9fab5169877615541990468f7fdfa7";
-var authToken  = "e15825c04e4b2f58df5d8d0f4521c7d5"
-// var authToken = "";
+// var authToken  = "e15825c04e4b2f58df5d8d0f4521c7d5" //old
+var authToken = process.env.TWILIO_TOKEN;
 
 
 
@@ -27,7 +27,7 @@ module.exports.run = function(){
       _.forEach(reminder4SMS, function(array){
         var user = array[0].parent().parent()
         var smsAlerts = array.length
-        if (user.firstName == "Kareem"){
+        if (user.firstName == "testinguser"){
           sendSMS(user, smsAlerts)
         }
         // if (user.cellNumber) {
@@ -38,13 +38,13 @@ module.exports.run = function(){
 }
 
 function sendSMS(user, r4SMS){
-//   var textBody = `Hi, ${user.firstName}. You have ${r4SMS} medication reminder${r4SMS = 1 ? "" : "s"}. Please log in to your account for more infomation.`
-//   client.messages.create({
-//     // to: user.cellNumber,
-//     to: user.cellNumber,
-//     from: "+14242034815",
-//     body: textBody,
-//   }, function(err, message) {
-//     console.log(err, message);
-//   });
+  var textBody = `Hi, ${user.firstName}. You have ${r4SMS} medication reminder${r4SMS = 1 ? "" : "s"}. Please log in to your account for more infomation.`
+  client.messages.create({
+    // to: user.cellNumber,
+    to: user.cellNumber,
+    from: "+14242034815",
+    body: textBody,
+  }, function(err, message) {
+    console.log(err, message);
+  });
 }
